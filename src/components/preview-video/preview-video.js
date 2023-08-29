eccbde61a5d9625868734a2a9dcbf8fd41eb556e
Sawarni Swaroop <sawarni-pt4114@csez.zohocorpin.com>
2023-08-29 20:55:58 +0530

    Preview video right side done.

--- src/components/preview-video/preview-video.js
+++ src/components/preview-video/preview-video.js
@@ -1,9 +1,10 @@
 import './preview-video.css'
 import { videos } from '../../assets';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 const PreviewVideo = () => {
     const [ended, setEnded] = useState(false);
+    const [muted, setMuted] = useState(true);
     const videoRef = useRef(null);
     const video = videos[12];  // Change this by passing props...
 
@@ -11,14 +12,24 @@ const PreviewVideo = () => {
         setEnded(true);
     }
 
-    const onPlay = () => {
-        console.log("Play");
+    const onClickControl = () =>{
+        if(ended){
+            setEnded(false);
+        }else{
+            if(muted){
+                videoRef.current.muted = false;
+                setMuted(false);
+            }else{
+                videoRef.current.muted = true;
+                setMuted(true);
+            }
+        }
     }
 
     return (
         <div id='preview-video'>
             {!ended ? 
-                <video onEnded={onEnded} onPlay={onPlay} ref={videoRef} id='preview-video-video' autoPlay muted>
+                <video onEnded={onEnded} ref={videoRef} id='preview-video-video' autoPlay muted={muted}>
                     <source src={video.trailer} />
                 </video> : 
                 <img onClick={() => setEnded(false)} id='preview-video-img' src={video.previewImg} alt="Nothing to Show" />
@@ -28,9 +39,21 @@ const PreviewVideo = () => {
                 <div id='preview-video-meta-left'>
                     <div id='preview-video-logo'></div>
                     <div id='preview-video-desc'></div>
-                    <div id='preview-logo-info'></div>
+                    <div id='preview-video-info'></div>
                 </div>
-                <div id='preview-video-meta-right'></div>
+                <div id='preview-video-meta-right'>
+                    <div id='preview-video-control' onClick={onClickControl}>
+                        {
+                            (ended) ?
+                                <img className='preview-video-control-img' src='./assets/icons/replay-icon.png' alt='Replay' /> :
+                                
+                                (muted) ? 
+                                    <img className='preview-video-control-img' src='./assets/icons/sound-off-icon.png' alt='Unmute' /> :
+                                    <img className='preview-video-control-img' src='./assets/icons/sound-on-icon.png' alt='Mute' />
+                        }
+                    </div>
+                    <div id='preview-video-rated'>{video.rated}</div>
+                </div>
             </div>
         </div>
     )

1edea0fc62be19355e6a71b116fab6bbbc62c043
Sawarni Swaroop <sawarni-pt4114@csez.zohocorpin.com>
2023-09-10 10:53:01 +0530

    Adding and removing elements to array when side bar is clicked.

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,50 +1,60 @@
 import './section.css'
 import { videos } from '../../assets';
 import SectionVideo from '../section-video/section-video';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getRootStyle } from '../../helper';
-import { ITEM_PER_SCREEN, VIDEO_CARD_WIDTH } from '../../constants';
+import { BOX_GAP, ITEM_PER_SCREEN, INITIAL_INDEX } from '../../constants';
+
+// TODO :: Have to hide side bar if length of videos is less than items per screen.
+// TODO :: Have to just pass videos array in sectionVideos, then while setting it we have to map it to <SectionVideo> with unique key.
+// TODO :: If we slide too much (50 times) to the right then width of video card is getting overflow from the slider.
 
 const Section = ({name}) => {
     const mainRef = useRef(null);
     const boxRefLeft = useRef(null);
-    const boxRefRight = useRef(null);
-    const video = videos[12];
-    const [sliderIndex, setSliderIndex] = useState(0);
-    const [sectionVideos, setSectionVideos] = useState([
-        <SectionVideo key='1' video={videos[11]} />, <SectionVideo key='2' video={video} />,
-        <SectionVideo key='3' video={video} />, <SectionVideo key='4' video={video} />,
-        <SectionVideo key='5' video={video} />, <SectionVideo key='6' video={video} />,
-        <SectionVideo key='7' video={video} />, <SectionVideo key='8' video={video} />,
-        <SectionVideo key='9' video={video} />, <SectionVideo key='10' video={video} />,
-        <SectionVideo key='11' video={video} />
+    const leftSideBar = useRef(null);
+    const initialIndex = new Number(getRootStyle(INITIAL_INDEX));
+    const [sliderIndex, setSliderIndex] = useState(initialIndex);
+    const [sectionVideos, setSectionVideos] = useState([      // These are just sample data...
+        <SectionVideo key='1' video={videos[0]} />, <SectionVideo key='2' video={videos[1]} />,
+        <SectionVideo key='3' video={videos[12]} />, <SectionVideo key='4' video={videos[3]} />,
+        <SectionVideo key='5' video={videos[4]} />, <SectionVideo key='6' video={videos[5]} />,
+        <SectionVideo key='7' video={videos[6]} />, <SectionVideo key='8' video={videos[7]} />,
+        <SectionVideo key='9' video={videos[8]} />, <SectionVideo key='10' video={videos[9]} />,
+        <SectionVideo key='11' video={videos[10]} />, <SectionVideo key='12' video={videos[11]} />,
     ]);
 
+
+    // On Click side bars...
     const onClickSlider = (increment) => {
         let index = sliderIndex;
-        const itemsPerScreen = getRootStyle(ITEM_PER_SCREEN) - 1;
+        const itemsPerScreen = new Number(getRootStyle(ITEM_PER_SCREEN));
         const length = sectionVideos.length;
-
+        const itemIndex = index - initialIndex;
         if(itemsPerScreen < sectionVideos.length){
 
-            if(increment) {  // On Click right side of slider...
+            // On Click right side of slider...
+            if(increment) {
                 setSectionVideos(sectionVideos => {
                     const toRet = [
-                        ...sectionVideos.slice(itemsPerScreen, length), 
-                        ...sectionVideos.slice(0, itemsPerScreen)
+                        ...sectionVideos,
+                        ...sectionVideos.slice(itemIndex*itemsPerScreen, itemIndex*itemsPerScreen + itemsPerScreen)
                     ];
                     console.log(toRet);
                     return toRet;
                 });
 
                 index += 1;
-                boxRefLeft.current.style.minWidth = `calc(${index} * 100% - ${getRootStyle(VIDEO_CARD_WIDTH)})`;
+                mainRef.current.style.transform = `translateX(calc((${index} * -100%))`;
+                leftSideBar.current.style.visibility = 'visible';
+                leftSideBar.current.children[0].style.display = 'inline'; 
+                setSliderIndex(index);
 
 
-            } else {    // On Click left side of slider...
+            // On Click left side of slider...
+            } else {
                 setSectionVideos(sectionVideos => {
                     const toRet = [
-                        ...sectionVideos.slice(length - itemsPerScreen, length), 
                         ...sectionVideos.slice(0, length - itemsPerScreen)
                     ];
                     console.log(toRet);
@@ -52,12 +62,9 @@ const Section = ({name}) => {
                 });
 
                 index -= 1;
-                // boxRefLeft.current.style.minWidth = `0px`;
-                // boxRefRight.current.style.minWidth = `calc(${index<0 ? -1*index : index} * 100% - ${getRootStyle(VIDEO_CARD_WIDTH)})`;
+                mainRef.current.style.transform = `translateX(calc(${index} * -100%))`;
+                setSliderIndex(index);
             }
-            mainRef.current.style.transform = `translateX(calc(${index} * -100%))`;
-            setSliderIndex(index);
-
         }
     }
 
@@ -67,15 +74,18 @@ const Section = ({name}) => {
                 <div id='section-title'>{name}</div>
             </div>
             <div id='section-menu-container'>
-                <div id='section-menu-left' onClick={() => onClickSlider(false)}></div>
+                <div id='section-menu-left' ref={leftSideBar} onClick={() => onClickSlider(false)}>
+                    <img className='side-bar-icon' src='./assets/icons/backword-arrow.png' alt='Back' />
+                </div>
                 <div id='section-menu' ref={mainRef}>
                     <div id='section-menu-box-left' ref={boxRefLeft} />
                     {
                         sectionVideos
                     }
-                    <div id='section-menu-box-right' ref={boxRefRight} />
-                    </div>
-                <div id='section-menu-right' onClick={() => onClickSlider(true)}></div>
+                </div>
+                <div id='section-menu-right' onClick={() => onClickSlider(true)}>
+                    <img className='side-bar-icon' src='./assets/icons/forword-arrow.png' alt='Next' />
+                </div>
             </div>
         </div>
     )
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const ITEM_PER_SCREEN = '--items-per-screen';
 export const MARGIN_VALUE = '--margin-value';
 export const BOX_GAP = '--box-gap';
 export const VIDEO_CARD_WIDTH = '--video-card-width';
+export const INITIAL_INDEX = '--initial-index'
